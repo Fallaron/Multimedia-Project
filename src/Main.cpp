b129aae601b7225c5decdc69d842fb93246bd3ff
Niklas Schröter <niklas.schroeter@gmx.de>
2017-06-13 15:54:10 +0200

    TEst

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <opencv2\core.hpp>
+#include <opencv2\imgproc.hpp>
+#include <opencv2\highgui.hpp>
+
+#define TEMPLATEWIDTH 64
+#define TEMPLATEHEIGHT 128
+
 using namespace std;
+using namespace cv;
+
 
 int main() {
 	cout << "Hello World";
 	getchar();
+}
+
+void slideOverImage(Mat img) {
+	int imgheight = img.size().height;
+	int imgwidth = img.size().width;
+
+
+
 }
