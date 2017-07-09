0ec5479dbd0200a7f56909cde11d5f7dde96f164
Niklas Schröter <niklas.schroeter@gmx.de>
2017-07-09 18:49:43 +0200

    updated python script

--- MMPScript.py
+++ MMPScript.py
@@ -1,6 +1,6 @@
-import os
-import sys;
-from scipy.optimize import curve_fit
+
+import sys
+
 import matplotlib.pyplot as plt
 import numpy
 
@@ -21,17 +21,21 @@ def main():
     c = 0
     array0x = []
     array0y = []
+    array0z = []
 
     array1x = []
     array1y = []
+    array1z = []
     for line in file.readlines():
         if c == 0:
             if (int(line) == 0):
                 arrayx = array0x
                 arrayy = array0y
+                arrayz = array0z
             if (int(line) == 1):
                 arrayx = array1x
                 arrayy = array1y
+                arrayz = array1z
         if c == 1:
             treshold = float(line)
         if c == 2:
@@ -41,24 +45,33 @@ def main():
         if c == 4:
             num_gBoxes = float(line)
 
-            falseRate = 1. - (false_pos / num_gBoxes)
-            missedRate = 1. - (truth / num_gBoxes)
+            falseRate = (false_pos / num_gBoxes)*100
+            missedRate = (1. - (truth / num_gBoxes))*100
 
             arrayx.append(falseRate)
             arrayy.append(missedRate)
+            arrayz.append(treshold)
+
         c = (c + 1) % 5
     print array0y
     print array1y
     print array0x
     print array1x
-
+    fig, ax = plt.subplots();
     plt.ylabel('missed detection rate')
     plt.xlabel('false positive rate')
-    plt.plot(array0x, array0y, 'b-', label='SVM x.0')
+    ax.plot(array0x, array0y, '-bo', label='SVM x.0')
 
-    plt.ylabel('missed detection rate')
-    plt.xlabel('false positive rate')
-    plt.plot(array1x, array1y, 'r-', label='SVM x.1')
+    for X,Y,Z in zip(array0x,array0y,array0z):
+        ax.annotate('{}'.format(Z), xy=(X,Y), xytext=(-5, 5), ha='left',
+                textcoords='offset points')
+
+
+    ax.plot(array1x, array1y, '-ro', label='SVM x.1')
+
+    for X,Y,Z in zip(array1x,array1y,array1z):
+        ax.annotate('{}'.format(Z), xy=(X,Y), xytext=(-5, -25), ha='left',
+                textcoords='offset points')
     plt.legend(loc='upper right')
     plt.show()
 
